974fc6dc2ba3c3e70eebb677f91b1849dda249f9
Joren vanGoethem <jorenvangoethem@hotmail.com>
2022-08-25 23:48:38 +0200

    working RNN example

--- test1/src/cudaWrapper.cpp
+++ test1/src/cudaWrapper.cpp
@@ -1,5 +1,7 @@
 #include "cudaWrapper.hpp"
 
+#include <cudnn_backend.h>
+#include <cudnn_ops_infer.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
 
@@ -22,7 +24,7 @@ CudaWrapper::CudaWrapper() :
     // create the tensor descriptor
     cudnnDataType_t dtype = CUDNN_DATA_FLOAT;
     cudnnTensorFormat_t format = CUDNN_TENSOR_NCHW;
-    int n = 1, c = 1, h = 1, w = 10;
+    int n = 1, c = 5, h = 1, w = 17;
     int m_ElementCount = n*c*h*w;
     cudnnTensorDescriptor_t x_desc;
     cudnnCreateTensorDescriptor(&x_desc);
@@ -31,23 +33,25 @@ CudaWrapper::CudaWrapper() :
     // create the tensor
     cudaMallocManaged(&m_X, m_ElementCount * sizeof(float));
     for(int i=0;i<m_ElementCount;i++) 
-        m_X[i] = i * 1.00f;
+        m_X[i] = i * 0.10f - 3;
     std::cout << "Original array: "; 
     for(int i=0;i<m_ElementCount;i++) 
         std::cout << m_X[i] << " ";
+    std::cout << '\n';
+    
 
     // create activation function descriptor
-    float alpha[1] = {1};
-    float beta[1] = {0.0};
-    cudnnActivationDescriptor_t sigmoid_activation;
-    cudnnActivationMode_t mode = CUDNN_ACTIVATION_SIGMOID;
+    float alpha[1] = {0};
+    float beta[1] = {0};
+    cudnnActivationDescriptor_t relu_activation;
+    cudnnActivationMode_t mode = CUDNN_ACTIVATION_RELU;
     cudnnNanPropagation_t prop = CUDNN_NOT_PROPAGATE_NAN;
-    cudnnCreateActivationDescriptor(&sigmoid_activation);
-    cudnnSetActivationDescriptor(sigmoid_activation, mode, prop, 0.0f);
+    cudnnCreateActivationDescriptor(&relu_activation);
+    cudnnSetActivationDescriptor(relu_activation, mode, prop, 0.0f);
 
-    cudnnActivationForward(
+    cudnnStatus_t result = cudnnActivationForward(
         m_Handle,
-        sigmoid_activation,
+        relu_activation,
         alpha,
         x_desc,
         m_X,
@@ -56,15 +60,17 @@ CudaWrapper::CudaWrapper() :
         m_X
     );
 
-    spdlog::info("Destroyed cuDNN handle.");
+    std::cout << cudnnGetErrorString(result) << '\n';
+
     std::cout << "New array: ";
     for(int i=0;i<m_ElementCount;i++) 
         std::cout << m_X[i] << " ";
-    std::cout << std::endl;
+    std::cout << '\n';
     cudaFree(m_X);
 }
 
 CudaWrapper::~CudaWrapper()
 {
+    spdlog::info("Destroyed cuDNN handle.");
     cudnnDestroy(m_Handle);
 }
